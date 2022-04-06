189cd0033cb3a3a01050a35362fd61be5efc460b
Gabriel Domingos <gabrielgdomingos@gmail.com>
2022-04-06 15:33:07 +0000

    Adicionado serializer para exibir campos específicos do recurso Hero

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,5 +1,5 @@
 class HeroesController < ApplicationController
-  before_action :set_hero, only: [:show, :update, :destroy]
+  before_action :set_hero, only: %i[show update destroy]
 
   # GET /heroes
   def index
@@ -39,13 +39,14 @@ class HeroesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_hero
-      @hero = Hero.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def hero_params
-      params.require(:hero).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_hero
+    @hero = Hero.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def hero_params
+    params.require(:hero).permit(:name)
+  end
 end
