7349bb396239e2c135db97a23d87190b0f311a77
Gabriel Domingos <gabrielgdomingos@gmail.com>
2022-04-07 22:20:10 +0000

    Adicionado testes de request para Hero

--- spec/models/hero_model_spec.rb
+++ spec/models/hero_model_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Hero, type: :model do
-  
   let(:name) { 'Thor' }
 
   subject { Hero.new(name: 'Thor') }
@@ -14,11 +13,11 @@ RSpec.describe Hero, type: :model do
 
   describe 'scopes' do
     it '.search' do
-      expect(Hero.where('LOWER(name) LIKE ?', "%#{:name.downcase}%")).to eq Hero.search(:name)
+      expect(Hero.where('LOWER(name) LIKE ?', "%#{name.downcase}%").to_sql).to eq Hero.search(name).to_sql
     end
 
     it '.sorted_by_name' do
-      expect(Hero.order(:name)).to eq Hero.sorted_by_name
+      expect(Hero.order(:name).to_sql).to eq Hero.sorted_by_name.to_sql
     end
   end
 end
